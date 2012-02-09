f3692d6cd82e08e8156ae4aaeeb0965f6c3cb771
John Prince <jtprince@gmail.com>
2012-02-08 23:27:28 -0700

    progress

--- bin/lipidomic-search.rb
+++ bin/lipidomic-search.rb
@@ -5,28 +5,32 @@ require 'ms/lipid/search'
 require 'ms/lipid/search/query'
 require 'ms/lipid_maps'
 
-if ARGV.size != 2
-  puts "need: lipimaps mzML "
+class Sample
+  attr_accessor :file
+  attr_accessor :spectrum
+  def initialize(file)
+    @file = file
+    @spectrum = merge_ms1_spectra(file)
+  end
+
+  # returns a single spectrum object
+  def merge_ms1_spectra(file)
+    spectra = []
+    MS::Mzml.foreach(file) do |spectrum|
+      spectra << spectrum if spectrum.mzs.size > 1000  # <<<<<<------ kludge for ms_level == 1
+    end
+    MS::Spectrum.merge(spectra, :bin_width => 3, :bin_unit => :ppm)
+  end
+end
+
+if ARGV.size < 2
+  puts "usage: #{File.basename(__FILE__)} lipidmaps.tsv <file>.mzML ..."
   exit
 end
 
-(lipidmaps, file) = ARGV
-
-spectra = []
-MS::Mzml.foreach(file) do |spectrum|
-  spectra << spectrum if spectrum.mzs.size > 700  # <<<<<<------ kludge for ms_level == 1
-end
+(lipidmaps, *files) = ARGV
 
 $VERBOSE = 5
-
-master_spectrum = MS::Spectrum.merge(spectra, :bin_width => 1, :bin_unit => :ppm)
-
-puts "LOOKING AT MASTER:"
-p master_spectrum.mzs.size
-p master_spectrum.mzs
-p master_spectrum.mzs.size
-p master_spectrum.intensities
-abort 'DONE MAKING MASTER SPECTRUM'
 
 proton = MS::Lipid::Modification.new(:proton)
 h2o_loss = MS::Lipid::Modification.new(:water, :loss => true)
@@ -41,9 +45,15 @@ end.flatten(1)
 
 searcher = MS::Lipid::Search.new(queries, :ppm => false)
 
-hit_groups = searcher.search(spectra[3].mzs[200,20])
+files.each do |file|
+  puts "/\"*80
+  puts "FILE: #{file}"
+  sample = Sample.new(file)
 
-hit_groups.map(&:first).sort_by(&:pvalue).each do |hit|
-  puts "="*80
-  p hit
+  hit_groups = searcher.search(sample.spectrum)
+
+  hit_groups.map(&:first).sort_by(&:pvalue).each do |hit|
+    puts "="*80
+    p hit
+  end
 end
